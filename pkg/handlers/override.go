3554c51f6e8743fa2e2fc85bd75f45a4ab4d177a
Mike Davis <mike@optimizely.com>
2020-03-10 11:45:04 -0700

    Add test for invalid payload.

--- pkg/handlers/override.go
+++ pkg/handlers/override.go
@@ -42,8 +42,8 @@ func Override(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var body OverrideBody
-	if ParseRequestBody(r, &body) != nil {
-		RenderError(err, http.StatusInternalServerError, w, r)
+	if parseErr := ParseRequestBody(r, &body); parseErr != nil {
+		RenderError(parseErr, http.StatusBadRequest, w, r)
 		return
 	}
 
@@ -72,7 +72,7 @@ func Override(w http.ResponseWriter, r *http.Request) {
 	logger.Debug().Str("experimentKey", experimentKey).Str("variationKey", body.VariationKey).Msg("setting override")
 	wasSet, err := optlyClient.SetForcedVariation(experimentKey, body.UserID, body.VariationKey)
 	switch {
-	case err == optimizely.ErrEntityNotFound:
+	case errors.Is(err, optimizely.ErrEntityNotFound):
 		RenderError(err, http.StatusBadRequest, w, r)
 	case err != nil:
 		RenderError(err, http.StatusInternalServerError, w, r)
